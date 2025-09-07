d9cf9cf94653c6782554a6bfadba0921578eccf2
Hari Om Ahlawat <hariomahlawat@outlook.com>
2025-09-07 10:07:26 +0530

    Enable multi-role user management

--- Areas/Admin/Pages/Users/Create.cshtml.cs
+++ Areas/Admin/Pages/Users/Create.cshtml.cs
@@ -1,8 +1,9 @@
-using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using ProjectManagement.Services;
 
 namespace ProjectManagement.Areas.Admin.Pages.Users
@@ -10,45 +11,43 @@ namespace ProjectManagement.Areas.Admin.Pages.Users
     [Authorize(Roles = "Admin")]
     public class CreateModel : PageModel
     {
-        private readonly IUserManagementService _userService;
-        public CreateModel(IUserManagementService userService) => _userService = userService;
+        private readonly IUserManagementService _users;
 
-        public IList<string> Roles { get; private set; } = new List<string>();
+        public CreateModel(IUserManagementService users) => _users = users;
 
         [BindProperty] public InputModel Input { get; set; } = new();
+        public IList<string> Roles { get; private set; } = new List<string>();
 
         public class InputModel
         {
-            [Required, StringLength(32, MinimumLength = 3)]
+            [Required, Display(Name = "Username")]
+            [StringLength(32, MinimumLength = 3)]
             [RegularExpression(@"^[a-zA-Z0-9_.-]+$", ErrorMessage = "Only letters, numbers, dot, underscore and hyphen.")]
             public string UserName { get; set; } = string.Empty;
 
-            [Required, StringLength(100, MinimumLength = 8)]
-            [DataType(DataType.Password)]
+            [Required, DataType(DataType.Password)]
+            [StringLength(100, MinimumLength = 8)]
             public string Password { get; set; } = "ChangeMe!123";
 
-            [Required]
-            public string Role { get; set; } = string.Empty;
+            [Required, Display(Name = "Roles")]
+            public List<string> Roles { get; set; } = new();
         }
 
-        public async Task OnGetAsync()
-        {
-            Roles = await _userService.GetRolesAsync();
-        }
+        public async Task OnGetAsync() => Roles = await _users.GetRolesAsync();
 
         public async Task<IActionResult> OnPostAsync()
         {
-            Roles = await _userService.GetRolesAsync();
+            Roles = await _users.GetRolesAsync();
             if (!ModelState.IsValid) return Page();
 
-            var result = await _userService.CreateUserAsync(Input.UserName, Input.Password, Input.Role);
-            if (result.Succeeded)
+            var res = await _users.CreateUserAsync(Input.UserName, Input.Password, Input.Roles);
+            if (res.Succeeded)
             {
                 TempData["ok"] = "User created.";
                 return RedirectToPage("Index");
             }
 
-            foreach (var e in result.Errors) ModelState.AddModelError(string.Empty, e.Description);
+            foreach (var e in res.Errors) ModelState.AddModelError(string.Empty, e.Description);
             return Page();
         }
     }
