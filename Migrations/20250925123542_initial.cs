4461c391e5db54773675dc044a4d05f8121bfa9d
Hari Om Ahlawat <hariomahlawat@gmail.com>
2025-09-25 18:20:45 +0530

    migration

---
+++ Migrations/20250925123542_initial.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "ProjectComments",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EditedOn",
+                table: "ProjectComments",
+                type: "timestamp without time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedOn",
+                table: "ProjectComments",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UploadedOn",
+                table: "ProjectCommentAttachments",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "Events",
+                type: "boolean",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldDefaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectComments_CreatedByUserId",
+                table: "ProjectComments",
+                column: "CreatedByUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectComments_EditedByUserId",
+                table: "ProjectComments",
+                column: "EditedByUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectCommentAttachments_UploadedByUserId",
+                table: "ProjectCommentAttachments",
+                column: "UploadedByUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectCommentAttachments_AspNetUsers_UploadedByUserId",
+                table: "ProjectCommentAttachments",
+                column: "UploadedByUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectComments_AspNetUsers_CreatedByUserId",
+                table: "ProjectComments",
+                column: "CreatedByUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectComments_AspNetUsers_EditedByUserId",
+                table: "ProjectComments",
+                column: "EditedByUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectCommentAttachments_AspNetUsers_UploadedByUserId",
+                table: "ProjectCommentAttachments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectComments_AspNetUsers_CreatedByUserId",
+                table: "ProjectComments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectComments_AspNetUsers_EditedByUserId",
+                table: "ProjectComments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProjectComments_CreatedByUserId",
+                table: "ProjectComments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProjectComments_EditedByUserId",
+                table: "ProjectComments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProjectCommentAttachments_UploadedByUserId",
+                table: "ProjectCommentAttachments");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "ProjectComments",
+                type: "boolean",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldDefaultValue: false);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EditedOn",
+                table: "ProjectComments",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedOn",
+                table: "ProjectComments",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UploadedOn",
+                table: "ProjectCommentAttachments",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "Events",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+        }
+    }
+}
