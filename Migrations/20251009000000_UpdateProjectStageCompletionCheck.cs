99f94f6d8905a86c7aebe03d000941c2a318c9f8
Hari Om Ahlawat <hariomahlawat@gmail.com>
2025-09-26 14:28:53 +0530

    error fix

--- Migrations/20251009000000_UpdateProjectStageCompletionCheck.cs
+++ Migrations/20251009000000_UpdateProjectStageCompletionCheck.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using ProjectManagement.Data;
 
@@ -12,16 +13,16 @@ namespace ProjectManagement.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql(@"ALTER TABLE \"ProjectStages\" DROP CONSTRAINT IF EXISTS \"CK_ProjectStages_CompletedHasDate\";");
+            migrationBuilder.Sql($"ALTER TABLE \"ProjectStages\" DROP CONSTRAINT IF EXISTS \"CK_ProjectStages_CompletedHasDate\";");
 
-            migrationBuilder.Sql(@"ALTER TABLE \"ProjectStages\" ADD CONSTRAINT \"CK_ProjectStages_CompletedHasDate\" CHECK (\"Status\" <> 'Completed' OR (\"CompletedOn\" IS NOT NULL AND \"ActualStart\" IS NOT NULL));");
+            migrationBuilder.Sql($"ALTER TABLE \"ProjectStages\" ADD CONSTRAINT \"CK_ProjectStages_CompletedHasDate\" CHECK (\"Status\" <> 'Completed' OR (\"CompletedOn\" IS NOT NULL AND \"ActualStart\" IS NOT NULL));");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql(@"ALTER TABLE \"ProjectStages\" DROP CONSTRAINT IF EXISTS \"CK_ProjectStages_CompletedHasDate\";");
+            migrationBuilder.Sql($"ALTER TABLE \"ProjectStages\" DROP CONSTRAINT IF EXISTS \"CK_ProjectStages_CompletedHasDate\";");
 
-            migrationBuilder.Sql(@"ALTER TABLE \"ProjectStages\" ADD CONSTRAINT \"CK_ProjectStages_CompletedHasDate\" CHECK (NOT(\"Status\" = 'Completed' AND \"CompletedOn\" IS NULL));");
+            migrationBuilder.Sql($"ALTER TABLE \"ProjectStages\" ADD CONSTRAINT \"CK_ProjectStages_CompletedHasDate\" CHECK (NOT(\"Status\" = 'Completed' AND \"CompletedOn\" IS NULL));");
         }
     }
 }
