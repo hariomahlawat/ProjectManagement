53bb9029ce876ee6236ef0030b8311665a76c6d1
Hari Om Ahlawat <hariomahlawat@outlook.com>
2025-11-02 15:17:57 +0530

    Fix range-processing file responses

--- Areas/DocumentRepository/Pages/Documents/View.cshtml.cs
+++ Areas/DocumentRepository/Pages/Documents/View.cshtml.cs
@@ -37,6 +37,11 @@ public class ViewModel : PageModel
         };
 
         Response.Headers["Content-Disposition"] = contentDisposition.ToString();
-        return File(stream, "application/pdf", fileDownloadName: document.OriginalFileName, enableRangeProcessing: true);
+
+        return new FileStreamResult(stream, "application/pdf")
+        {
+            FileDownloadName = document.OriginalFileName,
+            EnableRangeProcessing = true
+        };
     }
 }
